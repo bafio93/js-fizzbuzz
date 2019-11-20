f4a9270f93bd684d07b51cb88bfbfb9165014b1a
Previtali_Fabio <fabprev93@gmail.com>
2019-11-20 09:46:44 +0100

    Better

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 //Creation of the list: we'll use a list of n numbers (in this case n = 100).
 var n = 100;
 for (var i = 1; i <= n; i++) {
-    if ((i%3 == 0) && (i%15 != 0)) {
-        console.log("Fizz");
+    if (i%15 == 0) {
+        console.log("FizzBuzz");
     }
-    else if ((i%5 == 0) && (i%15 != 0)) {
+    else if (i%5 == 0) {
         console.log("Buzz");
     }
-    else if (i%15 == 0) {
-        console.log("FizzBuzz");
+    else if (i%3 == 0) {
+        console.log("Fizz");
     }
     else {
         console.log(i);
