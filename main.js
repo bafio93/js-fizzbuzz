2fb7c3bc86a265d4caadf199e7ccbe487c7ca68a
Previtali_Fabio <fabprev93@gmail.com>
2019-11-20 09:58:36 +0100

    Even more user-friendly :D

--- main.js
+++ main.js
@@ -2,15 +2,15 @@
 var n = 100;
 for (var i = 1; i <= n; i++) {
     if (i%15 == 0) {
-        console.log("FizzBuzz");
+        document.writeln("FizzBuzz <br>");
     }
     else if (i%5 == 0) {
-        console.log("Buzz");
+        document.writeln("Buzz <br>");
     }
     else if (i%3 == 0) {
-        console.log("Fizz");
+        document.writeln("Fizz <br>");
     }
     else {
-        console.log(i);
+        document.writeln(i + "<br>");
     }
 }
